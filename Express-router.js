90d10b67ff3cffd2dc2bc90e18870f14cca71197
Rachell Sincere <rachellsincere@gmail.com>
2019-08-13 18:20:09 -0400

    created base Endpoints for the specified routes

--- Express-router.js
+++ Express-router.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
 
 //Add middleware here if needed
-//..
+//...
 
 // for url beginning with /api/posts so 
+router.post('/', (req, res) => {
+    
+});
 
+router.post('/:id', (req, res) => {
+    
+});
 
-module.exports = router;
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/:id', (req, res) => {
+    
+});
+
+router.get('/:id/comments', (req, res) => {
+    
+});
+
+router.delete('/:id', (req, res) => {
+    
+});
+
+router.put('/:id', (req, res) => {
+    
+});
+module.exports = router;  
